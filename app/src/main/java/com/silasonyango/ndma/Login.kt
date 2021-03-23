5dcb0888b6b3c5eaa555061ae60da2a50de3131e
Silas Onyango <silas.onyango93@gmail.com>
2021-03-23 23:52:55 +0300

    login ui

---
+++ app/src/main/java/com/silasonyango/ndma/Login.kt
@@ -0,0 +1,12 @@
+package com.silasonyango.ndma
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
